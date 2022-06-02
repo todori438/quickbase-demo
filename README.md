1be7e5e1217a3edf6969e7932bb4333917b26412
Todor Ivanov <todorivanov@192.168.0.101>
2022-06-02 23:54:26 +0300

    added terraform code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # quickbase-demo
+
+# prereqs
+
+setup aws account and configure credentials or assume role
+make sure terraform is installed
+
+# clone repo to get the terraform files
+git clone https://github.com/todori438/quickbase-demo.git
+
+# cd into the repo and enter terraform-ec2-module
+terraform init
+terraform plan
+terraform apply
+
+this will deploy ec2 instance and will execute userdata to deploy docker and to run a simple docker app. the app could be found in this repo, but /app folder
+
---
+++ terraform-ec2-module/ec2-instance.tf
@@ -0,0 +1,75 @@
+module "dev_ssh_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "ec2_sg"
+  description = "Security group for ec2_sg"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["79.100.158.144/32"] 
+  ingress_rules       = ["ssh-tcp"]
+}
+
+module "ec2_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "ec2_sg"
+  description = "Security group for ec2_sg"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5001
+      to_port     = 5001
+      protocol    = "tcp"
+    }
+  ]
+  egress_rules        = ["all-all"]
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro"
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo yum install amazon-linux-extras install docker git -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo git clone https://github.com/todori438/quickbase-demo.git /tmp/deploy
+    sudo docker build /tmp/deploy/app -t hello-app
+    sudo docker run --detach --publish=5001:80 --name=hello-app hello-app
+  EOF
+
+  vpc_security_group_ids = [
+    module.ec2_sg.security_group_id,
+    module.dev_ssh_sg.security_group_id
+  ]
+
+  tags = {
+    project = "hello-world"
+  }
+
+  monitoring              = true
+  disable_api_termination = false
+  ebs_optimized           = true
+  key_name                = "test"
+}
