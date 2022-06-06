92592b97bb90fc031d1fde2497aef6045d65fa81
Todor Ivanov <todorivanov@192.168.0.101>
2022-06-06 21:35:23 +0300

    test

--- terraform-ec2-module/ec2-instance.tf
+++ terraform-ec2-module/ec2-instance.tf
@@ -37,6 +37,40 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
+resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro"
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo yum install amazon-linux-extras install docker git -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    sudo git clone https://github.com/todori438/quickbase-demo.git /tmp/deploy
+    sudo docker build /tmp/deploy/jenkins -t jenkins
+    sudo docker run --detach --publish=8080:80 --name=jenkins jenkins   
+  EOF
+
+  vpc_security_group_ids = [
+    module.ec2_sg.security_group_id,
+    module.dev_ssh_sg.security_group_id
+  ]
+
+  tags = {
+    project = "jenkins"
+  }
+
+  monitoring              = true
+  disable_api_termination = false
+  ebs_optimized           = true
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t3.micro"
@@ -52,8 +86,6 @@ resource "aws_instance" "web" {
     sudo yum install amazon-linux-extras install docker git -y
     sudo service docker start
     sudo usermod -a -G docker ec2-user
-    sudo curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
     sudo git clone https://github.com/todori438/quickbase-demo.git /tmp/deploy
     sudo docker build /tmp/deploy/app -t hello-app
     sudo docker run --detach --publish=5001:80 --name=hello-app hello-app
@@ -71,5 +103,4 @@ resource "aws_instance" "web" {
   monitoring              = true
   disable_api_termination = false
   ebs_optimized           = true
-  key_name                = "test"
 }
